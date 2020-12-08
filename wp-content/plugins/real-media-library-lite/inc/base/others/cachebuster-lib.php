f6a93966e67fd72c5c695757f7d75e4fcbff11a3
MaximUgarov <maximkaxaxaxa@mail.ru>
2020-12-08 17:04:24 +0300

    first commit

---
+++ wp-content/plugins/real-media-library-lite/inc/base/others/cachebuster-lib.php
@@ -0,0 +1,13 @@
+<?php
+/* This file was automatically generated (Tue Nov 24 2020 10:21:22 GMT+0000 (Coordinated Universal Time)). */
+return array(
+	'es6-shim' => '0.35.5',
+	'es7-shim' => '6.0.0',
+	'i18n-react' => '0.7.0',
+	'mobx' => '4.15.7',
+	'mobx-state-tree' => '3.17.2',
+	'react' => '16.13.1',
+	'react-aiot' => '1.5.3',
+	'react-dom' => '16.13.1',
+	'wp-media-picker' => '0.7.2'
+);
---
+++ wp-content/plugins/real-media-library-lite/inc/base/others/cachebuster.php
@@ -0,0 +1,11 @@
+<?php
+/* This file was automatically generated (Tue Nov 24 2020 10:21:22 GMT+0000 (Coordinated Universal Time)). */
+return array(
+	'src/public/dist/rml_gutenberg.lite.js' => 'fd2ddfe644aa0d19603bdc226ced39ea',
+	'src/public/dist/rml_gutenberg.pro.js' => '3fcf6e26174e2bf42f7956a3b642f799',
+	'src/public/dist/rml_shortcode.lite.js' => '78ff08ab98544a7d6d43e27c13e8cdf2',
+	'src/public/dist/rml_shortcode.pro.js' => '954f2e1c9e8ae4ea98367fd1d2323ed8',
+	'src/public/dist/rml.lite.js' => 'a41d3cc641d0de1f7c063d7cd3aaced3',
+	'src/public/dist/rml.pro.js' => '61125d2787d9069da47261d5ac149139',
+	'src/public/dist/rml.css' => '2399cfca36564fa348d31942951e8a71',
+);
